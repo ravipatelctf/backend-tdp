2fe3ae210a50aaacd99e7eca3006c50eabf8d086
Ravi Shankar Patel <ravipatelctf@gmail.com>
2025-10-18 16:08:34 +0530

    feat: add login with github and google using oauth

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { initializeDatabase } from "./db/db.connect.js";
 // import fs from "fs";
 import Tutor from "./models/tutor.models.js";
 import express from "express";
+import axios from "axios";
 
 const app = express()
 app.use(express.json());
@@ -15,6 +16,75 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 initializeDatabase();
+
+//---------------------------------------------------------------------------------------------------------
+// oauth
+app.get("/auth/github", (req, res) => {
+    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=user,repo,security_events`;
+    res.redirect(githubAuthUrl);
+})
+
+app.get("/auth/github/callback", async (req, res) => {
+    const { code } = req.query;
+    if (!code) {
+        return res.status(400).send("Authorization code not provided.");
+    }
+    try {
+        const tokenResponse = await axios.post(`https://github.com/login/oauth/access_token`, {
+            client_id: process.env.GITHUB_CLIENT_ID,
+            client_secret: process.env.GITHUB_CLIENT_SECRET,
+            code
+        }, {
+            headers: {
+                Accept: "application/json"
+            }
+        }
+    );
+
+        const accessToken = tokenResponse.data.access_token;
+        res.cookie("access_token", accessToken);
+        return res.redirect(`${process.env.FRONTEND_URL}/v1/profile/github`);
+
+    } catch (error) {
+        res.status(500).json({error});
+    }
+})
+
+app.get("/auth/google", (req, res) => {
+    const googleAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=http://localhost:${PORT}/auth/google/callback&response_type=code&scope=profile email`;
+    res.redirect(googleAuthUrl);
+})
+
+app.get("/auth/google/callback", async (req, res) => {
+    const { code } = req.query;
+    if (!code) {
+        return res.status(400).send("Authorization code not provided.");
+    }
+    let accessToken;
+    try {
+        const tokenResponse = await axios.post(`https://oauth2.googleapis.com/token`, {
+            client_id: process.env.GOOGLE_CLIENT_ID,
+            client_secret: process.env.GOOGLE_CLIENT_SECRET,
+            code,
+            grant_type: "authorization_code",
+            redirect_uri: `http://localhost:${PORT}/auth/google/callback`,
+        },{
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        });
+
+        accessToken = tokenResponse.data.access_token;
+        res.cookie("access_token", accessToken);
+        return res.redirect(`${process.env.FRONTEND_URL}/v1/profile/google`);
+
+    } catch (error) {
+        res.status(500).json({error})
+    }
+})
+
+
+//---------------------------------------------------------------------------------------------------------
 
 //---------------------------------------------------------------------------------------------------------
 // const jsonData = fs.readFileSync("tutors.json", "utf-8");
